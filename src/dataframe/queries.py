01ef529de84d3cdc7d4660e34a9a1d011f7403d0
prinston27 <71750924+prinston27@users.noreply.github.com>
2022-11-02 20:36:39 +1100

    Update queries.py

--- src/dataframe/queries.py
+++ src/dataframe/queries.py
@@ -24,7 +24,21 @@ def get_numeric_tables_query(schema_name, table_name):
     -> (type): description
 
     """
-    => To be filled by student
+    query = ("""select col.table_schema,col.table_name,col.ordinal_position as col_id,col.column_name,col.data_type,col.numeric_precision,col.numeric_scale
+        from information_schema.columns col
+        join information_schema.tables tab 
+        on tab.table_schema = col.table_schema
+        and tab.table_name = col.table_name
+        and tab.table_type = 'BASE TABLE'
+        where col.data_type in ('smallint', 'integer', 'bigint','decimal', 'numeric', 'real', 'double precision','smallserial', 'serial', 'bigserial', 'money')
+        and col.table_schema not in ('information_schema', 'pg_catalog')
+        and tab.table_name = '{0}'
+        and tab.table_schema = '{1}'
+        order by col.table_schema,
+        col.table_name,
+        col.ordinal_position;""".format(table_name,schema_name))
+    
+    return query
 
 def get_text_tables_query(schema_name, table_name):
     """
