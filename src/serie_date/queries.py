6e374840286de691123dba286ba21dcf852db56f
prinston27 <71750924+prinston27@users.noreply.github.com>
2022-11-09 02:38:40 +1100

    Update queries.py

--- src/serie_date/queries.py
+++ src/serie_date/queries.py
@@ -24,7 +24,24 @@ def get_min_date_query(schema_name, table_name, col_name):
     -> (type): description
 
     """
-    => To be filled by student
+    if len(schema_name)==0 and len(table_name)==0 and len(col_name)==0:
+        print('No arguments')
+        sys.exit(0)
+    elif table_name==None and col_name==None:
+        print('Empty table and column name string')
+        sys.exit(1)
+    elif table_name==None:
+        print('Empty table name string')
+        sys.exit(2)
+    elif col_name==None:
+        print('Empty column name string')
+        sys.exit(3)
+    return """
+        select min({col_name}) as EarliestDate
+        from {table_name}
+    """.format(col_name = col_name,table_name = table_name)
+    
+ 
 
 def get_weekend_count_query(schema_name, table_name, col_name):
     """
@@ -52,7 +69,24 @@ def get_weekend_count_query(schema_name, table_name, col_name):
     -> (type): description
 
     """
-    => To be filled by student
+    if len(schema_name)==0 and len(table_name)==0 and len(col_name)==0:
+        print('No arguments')
+        sys.exit(0)
+    elif table_name==None and col_name==None:
+        print('Empty table and column name string')
+        sys.exit(1)
+    elif table_name==None:
+        print('Empty table name string')
+        sys.exit(2)
+    elif col_name==None:
+        print('Empty column name string')
+        sys.exit(3)
+    return """ SELECT count({col_name})
+                FROM {table_name} 
+                WHERE EXTRACT(week FROM {col_name}) Not IN (0,5)
+                """.format(col_name = col_name,table_name = table_name)
+    
+    
 
 def get_1900_count_query(schema_name, table_name, col_name):
     """
@@ -80,4 +114,22 @@ def get_1900_count_query(schema_name, table_name, col_name):
     -> (type): description
 
     """
-    => To be filled by student
+    if len(schema_name)==0 and len(table_name)==0 and len(col_name)==0:
+        print('No arguments')
+        sys.exit(0)
+    elif table_name==None and col_name==None:
+        print('Empty table and column name string')
+        sys.exit(1)
+    elif table_name==None:
+        print('Empty table name string')
+        sys.exit(2)
+    elif col_name==None:
+        print('Empty column name string')
+        sys.exit(3)
+    return """
+                select count({col_name})
+                from {table_name} 
+                where {col_name} = '1900-01-01'
+    """.format(col_name = col_name,table_name = table_name)
+
+     
