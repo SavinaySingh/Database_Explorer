4826ea7b664b9aa9531fd488a942d0adf395163c
prinston27 <71750924+prinston27@users.noreply.github.com>
2022-11-09 05:26:17 +1100

    Update display.py

--- app/src/serie_date/display.py
+++ app/src/serie_date/display.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 from src.serie_date.logics import DateColumn
-
+from src.database.logics import PostgresConnector
+import pandas.io.sql as 
 
 def display_dates():
     """
@@ -13,23 +14,29 @@ def display_dates():
     --------------------
     Parameters
     --------------------
-    => To be filled by student
-    -> name (type): description
-
+    -> No parameters
     --------------------
     Pseudo-Code
     --------------------
-    => To be filled by student
-    -> pseudo-code
-
+    -> Create the sql query to select all the rows and columns from the table
+    -> Use pandas.sql.io to store these rows into a dataframe
+    -> Filter this dataframe to only store numeric columns
+    -> For each numerical column run get_summary_df function of DateColumn class to set the values of class attributes
+    -> Display the output in Streamlit application
     --------------------
     Returns
     --------------------
-    => To be filled by student
-    -> (type): description
-
+    None
     """
-    => To be filled by student
+    table_name='order_details'
+    sql='select * from {}'.format(table_name)
+    dat = sqlio.read_sql_query(sql, db.conn)
+    date = ['datetime64','timestamp without time zone','timestamp with time zone','time with time zone','time without time zone','interval', 'date']
+    df = dat.select_dtypes(include=datetime64)
+    date_cols=list(df.columns)
+    for cols in numeric_cols:
+        numeric=DateColumn(table_name='order_details',col_name=cols)
+        numeric.get_summary_df()
 
 def display_date(col_name, i):
     """
