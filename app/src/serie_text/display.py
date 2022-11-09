00bde3480b5d42d2683aae08a6e1f2ab64bcfede
SAGAR BHAGWATKAR <sagarbhagwatkar99@gmail.com>
2022-11-09 16:28:34 +1100

    Update display.py

--- app/src/serie_text/display.py
+++ app/src/serie_text/display.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from src.serie_text.logics import TextColumn
 
-def display_texts():
+def display_texts(table_name, schema_name):
     """
     --------------------
     Description
@@ -12,23 +12,33 @@ def display_texts():
     --------------------
     Parameters
     --------------------
-    => To be filled by student
-    -> name (type): description
+    -> schema_name (str): Name of the dataset schema 
+    -> table_name (str): Name of the dataset table 
 
     --------------------
     Pseudo-Code
     --------------------
-    => To be filled by student
-    -> pseudo-code
+    
+    load data -> convert data into dataframe -> select data having data type as string-> make a list of columns-> itterate through the list of columns-> expand each column and display relevent information for every text column
 
     --------------------
     Returns
     --------------------
-    => To be filled by student
+
     -> (type): description
 
     """
-    => To be filled by student
+    load_data = st.session_state.db.load_table(table_name, schema_name)
+    dataset = pd.DataFrame(load_data)
+    dataset = dataset.convert_dtypes().dtypes
+    dataset = dataset.loc[data['datatype']=='string']
+    col_list = list(data['index'])
+    count = 1
+    for name in col_list:
+        with st.expander(f"{count}. Column: {str(name)}", expanded=False):
+            col_serie = st.session_state.db.run_query(f"SELECT {name} FROM {schema_name}.{table_name};")
+            display_text(table_name, schema_name, name, col_serie)
+            count = count+1
 
 def display_text(col_name, i):
     """
@@ -40,20 +50,20 @@ def display_text(col_name, i):
     --------------------
     Parameters
     --------------------
-    => To be filled by student
-    -> name (type): description
+
+    -> schema_name (str): Name of the dataset schema 
+    -> table_name (str): Name of the dataset table
+    -> col_name (str): Name of the column
 
     --------------------
     Pseudo-Code
     --------------------
-    => To be filled by student
-    -> pseudo-code
-
+    -> use TextColumn class and give information about db,schema_name,table_name,col_name and serie and use set_data method to display all information of the column.
     --------------------
     Returns
     --------------------
-    => To be filled by student
     -> (type): description
 
     """
-    => To be filled by student
+    disp_text = TextColumn(db=st.session_state.db, schema_name=schema_name, table_name=table_name, col_name=col_name,  serie = pd.Series(i))
+    disp_text.set_data()
