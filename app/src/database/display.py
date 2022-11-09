5a27203899dff0bb06b389d766d2a2cf54527c38
SteffiTensingh <112261367+SteffiTensingh@users.noreply.github.com>
2022-11-09 09:02:07 +0800

    Update display.py

--- app/src/database/display.py
+++ app/src/database/display.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from logics import PostgresConnector
+from src.database.logics import PostgresConnector
 def display_db_connection_menu():
     P = PostgresConnector()
     """
@@ -7,25 +7,22 @@ def display_db_connection_menu():
     Description
     --------------------
     -> display_db_connection_menu (function): Function that displays the menu for connecting to a database and triggers the database connection
-
     --------------------
     Parameters
     --------------------
-    => To be filled by student
+    => User Name , Password, Database Host , Database port 
     -> name (type): description
-
     --------------------
     Pseudo-Code
     --------------------
-    => To be filled by student
+    => the form is prefilled by the parameters to connect the database and 
+    connect to submit the form by validating the creditials
     -> pseudo-code
-
     --------------------
     Returns
     --------------------
-    => To be filled by student
+    => prefilled form with the connect credentials
     -> (type): description
-
     """
     st.header('Streamlit application for performing data exploration on a database')
     st.title('Database connection details')
@@ -44,25 +41,29 @@ def connect_db():
     Description
     --------------------
     -> connect_db (function): Function that connects to a database and instantiate a PostgresConnector class accordingly
-
     --------------------
     Parameters
     --------------------
-    => To be filled by student
+    => Insstantiate the Postegreconnctor to connect the database
+    then declare a cursor to connect to the table and fetch the details
     -> name (type): description
-
     --------------------
     Pseudo-Code
     --------------------
-    => To be filled by student
-    -> pseudo-code
-
+    => 
+    P1 = PostgresConnector()
+    P1.open_connection()
+    P1.open_cursor()
+    P1.list_tables()
+    P1.load_table('public','django_migrations')
+    P1.get_table_schema('public','django_migrations')
+    P1.close_cursor()
+    P1.close_connection()
     --------------------
     Returns
     --------------------
     => To be filled by student
     -> (type): description
-
     """
 
     P1 = PostgresConnector()
@@ -80,28 +81,18 @@ def display_table_selection():
     Description
     --------------------
     -> display_table_selection (function): Function that displays the selection box for selecting the table to be analysed and triggers the loading of data (read_data())
-
     --------------------
     Parameters
     --------------------
     => To be filled by student
     -> name (type): description
-
     --------------------
     Pseudo-Code
     --------------------
     => To be filled by student
     -> pseudo-code
-
     --------------------
     Returns
     --------------------
     => To be filled by student
     -> (type): description
-
-    """
-
-
-
-
-
