ee666b889b88c6bfe2117c5c2d504389d0cf05f7
prinston27 <71750924+prinston27@users.noreply.github.com>
2022-11-09 19:44:41 +1100

    Update test_serie_date_queries.py

--- app/src/test/test_serie_date_queries.py
+++ app/src/test/test_serie_date_queries.py
@@ -3,7 +3,101 @@ import pandas as pd
 
 from src.serie_date.queries import *
 
-=> To be filled by student
+class TestGetMinDateQuery(unittest.TestCase):
+    def test_correct_args(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name='birth_date'
+        self.assertEqual(get_min_date_query(schema_name,table_name,col_name),'select count(*) from {} where {} <0'.format(table_name,col_name), 'Wrong Output')
+    def test_no_args(self):
+        with self.assertRaises(SystemExit) as cm:
+            get_min_date_query()
+        self.assertEqual(cm.exception.code, 0)
+    def test_empty_table_column(self):
+        schema_name='postgres'
+        table_name=None
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_min_date_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 1)
+    def test_empty_table(self):
+        schema_name='postgres'
+        table_name=None
+        col_name='birth_date'
+        with self.assertRaises(SystemExit) as cm:
+            get_min_date_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 2)
+    def test_empty_column(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_min_date_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 3)
 
+class TestGetWeekEndCountQuery(unittest.TestCase):
+    def test_correct_args(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name='birth_date'
+        self.assertEqual(get_weekend_count_query(schema_name,table_name,col_name),'select count(*) from {} where {} <0'.format(table_name,col_name), 'Wrong Output')
+    def test_no_args(self):
+        with self.assertRaises(SystemExit) as cm:
+            get_weekend_count_query()
+        self.assertEqual(cm.exception.code, 0)
+    def test_empty_table_column(self):
+        schema_name='postgres'
+        table_name=None
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_weekend_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 1)
+    def test_empty_table(self):
+        schema_name='postgres'
+        table_name=None
+        col_name='birth_date'
+        with self.assertRaises(SystemExit) as cm:
+            get_weekend_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 2)
+    def test_empty_column(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_weekend_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 3)
+
+class TestGet1900CountQuery(unittest.TestCase):
+    def test_correct_args(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name='birth_date'
+        self.assertEqual(get_1900_count_query(schema_name,table_name,col_name),'select count(*) from {} where {} <0'.format(table_name,col_name), 'Wrong Output')
+    def test_no_args(self):
+        with self.assertRaises(SystemExit) as cm:
+            get_1900_count_query()
+        self.assertEqual(cm.exception.code, 0)
+    def test_empty_table_column(self):
+        schema_name='postgres'
+        table_name=None
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_1900_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 1)
+    def test_empty_table(self):
+        schema_name='postgres'
+        table_name=None
+        col_name='birth_date'
+        with self.assertRaises(SystemExit) as cm:
+            get_1900_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 2)
+    def test_empty_column(self):
+        schema_name='postgres'
+        table_name='employees'
+        col_name=None
+        with self.assertRaises(SystemExit) as cm:
+            get_1900_count_query(schema_name,table_name,col_name)
+        self.assertEqual(cm.exception.code, 3)
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2)
